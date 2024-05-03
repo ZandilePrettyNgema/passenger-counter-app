2af4bec5b12719fd9a76e220a43c20ec2f2595ae
Zandile Pretty Ngema <zandilepretty01@gmail.com>
2024-05-03 18:14:09 +0200

    .

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let count = 0;
 let countEl = document.getElementById("count");
+let saveEl = document.getElementById("save");
+
 function increment() {
-  count = count + 1;
+  count += 1;
   countEl.innerHTML = count;
 }
+
+function save() {
+  let entries = " " + count + " - ";
+  saveEl.innerHTML += entries;
+  countEl.innerHTML -= count;
+  count = 0;
+}
+
+
